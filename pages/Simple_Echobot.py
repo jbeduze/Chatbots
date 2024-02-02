92ac1c5e1c4b4aa0c0a23eb6b24967a0daf33e0b
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-02-02 00:13:00 -0600

    Update Simple_Echobot.py

--- pages/Simple_Echobot.py
+++ pages/Simple_Echobot.py
@@ -10,18 +10,18 @@ with st.expander("Chatbot", expanded=True):
   if "messages" not in st.session_state:
       st.session_state.message = []
 #display chat message from history
-for message in st.session_state.message:
-  with st.chat_message(message["role"]):
-    st.markdown(messsage["content"])
+  for message in st.session_state.message:
+    with st.chat_message(message["role"]):
+      st.markdown(messsage["content"])
 #react to user input
-if prompt := st.chat_input():
-  with st.chat_message("user"):
-    st.markdown(prompt)
+  if prompt := st.chat_input():
+    with st.chat_message("user"):
+      st.markdown(prompt)
 #user message to chat history
-  st.session_state.messages.append({"role": "user", "content": prompt})
-
-  response = f"echo: {prompt}"
-  with st.chat_message("assistant"):
-    st.markdown(response)
-#assistant messsage to chat history  
-  st.session_state.messages.append({"role": "aassistant", "content": response})
+    st.session_state.messages.append({"role": "user", "content": prompt})
+  
+    response = f"echo: {prompt}"
+    with st.chat_message("assistant"):
+      st.markdown(response)
+  #assistant messsage to chat history  
+    st.session_state.messages.append({"role": "aassistant", "content": response})
