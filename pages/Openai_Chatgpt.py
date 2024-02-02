a8708231e1538d96fbfa3797d31a6c32ba31ca34
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-02-01 21:28:05 -0600

    Update Openai_Chatgpt.py

--- pages/Openai_Chatgpt.py
+++ pages/Openai_Chatgpt.py
@@ -25,10 +25,10 @@ with st.expander("Assistant created within OpenAI and called to Streamlit"):
   if "thread_id" not in st.session_state:
     st.session_state.thread_id = None
   
-  openai.api_key= secrets.toml["openai.api_key"]
+  openai.api_key= st.secrets["openai.api_key"]
   
   if st.button("Start Chat"):
-    st.session_stae.start_chat= True
+    st.session_state.start_chat= True
     thread = Client.beta.threads.create()
     st.session_state.thread_id = thread_id
     
