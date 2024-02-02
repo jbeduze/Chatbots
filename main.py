ff75c4d7197668692a056d5a98e2d873f3bd8fbf
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-02-01 18:48:40 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Check out all the different chatbots that we can build!")
+st.write("Navigate through the different pages to see what's possible- the Page's titles will help ellude to they're basic construction")
