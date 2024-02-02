c18fe4c5bd20eb97c8f0c6d4cf5f5124dbb1fd7f
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-02-01 20:03:49 -0600

    Update Openai_Chatgpt.py

--- pages/Openai_Chatgpt.py
+++ pages/Openai_Chatgpt.py
@@ -2,8 +2,17 @@ import streamlit as st
 
 st.header('OpenAI Chatbots')
 with st.expander("Decsription and features"):
-  st.write("Openai's chagpt software read by python in a streamlit UI")
-with st.expander("Assistant created within OpenAI and called to Streamlit"):
-  st.write('chat')
+  st.write("""Openai's chagpt software read by python in a streamlit UI.
+  
+  2 different examples are below. The main difference is where the directions for the chatbot originate.
+  
+  1st- from within the python code
+  
+  2nd- from within the OpenAI asssitant creator""")
 with st.expander("Assistant created within Streamlit"):
   st.write('chat')
+  assistant = client.beeta.assistant.create(
+    name="
+with st.expander("Assistant created within OpenAI and called to Streamlit"):
+  st.write('chat')
+
