98e6b5fdd3e3a920acafa6ea542f03d49f1007af
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-02-01 19:56:19 -0600

    Update Openai_Chatgpt.py

--- pages/Openai_Chatgpt.py
+++ pages/Openai_Chatgpt.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-st.header('OpenAI Chatbot')
+st.header('OpenAI Chatbots')
 with st.expander("Decsription and features"):
   st.write("Openai's chagpt software read by python in a streamlit UI")
+with st.expander("Assistant created within OpenAI and called to Streamlit"):
+  st.write('chat')
+with st.expander("Assistant created within Streamlit"):
+  st.write('chat')
