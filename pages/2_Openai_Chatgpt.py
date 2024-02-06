7d625ea33801b7e5cd029fc6834ae6848023f7dd
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-02-05 23:44:25 -0600

    Update 2_Openai_Chatgpt.py

--- pages/2_Openai_Chatgpt.py
+++ pages/2_Openai_Chatgpt.py
@@ -10,9 +10,9 @@ st.header('OpenAI Simple Chatbots')
 
 with st.expander("Description and features"):
     st.write("""Openai's chagpt software read by python in a streamlit UI.
-    2 different examples are below. The main difference is where the directions for the chatbot originate.
-    1st- from within the python code- Streamlit provides code for the build out of this app: https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps as well as the youtube video: https://www.youtube.com/watch?v=sBhK-2K9bUc&t=303s 
-    2nd- from within the OpenAI asssitant creator""")
+        2 different examples are below. The main difference is where the directions for the chatbot originate.
+        1st- from within the python code- Streamlit provides code for the build out of this app: https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps as well as the youtube video: https://www.youtube.com/watch?v=sBhK-2K9bUc&t=303s 
+        2nd- from within the OpenAI asssitant creator""")
 
 with st.expander("Assistant created within Streamlit"):
         # Initialize session state for openai_model
