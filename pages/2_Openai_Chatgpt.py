84aecf0032f3c9582367925e36e63e6e73249fc4
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-02-07 00:16:04 -0600

    Update 2_Openai_Chatgpt.py

--- pages/2_Openai_Chatgpt.py
+++ pages/2_Openai_Chatgpt.py
@@ -1,48 +1,83 @@
-import streamlit as st
-import openai
 from openai import OpenAI
-import time
+import streamlit as st
 
-st.header('OpenAI Simple Chatbots')
+st.title("ChatGPT-like clone")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+# import streamlit as st
+# import openai
+# from openai import OpenAI
+# import time
+
+# st.header('OpenAI Simple Chatbots')
                
-with st.expander("Description and Features"):
-  st.write("""Openai's chagpt software read by python in a streamlit UI.
-    2 different examples are below. The main difference is where the directions for the chatbot originate.
-    1st- from within the python code- Streamlit provides code for the build out of this app: https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps as well as the youtube video: https://www.youtube.com/watch?v=sBhK-2K9bUc&t=303s 
-    2nd- from within the OpenAI asssitant creator"""
-  )
+# with st.expander("Description and Features"):
+#   st.write("""Openai's chagpt software read by python in a streamlit UI.
+#     2 different examples are below. The main difference is where the directions for the chatbot originate.
+#     1st- from within the python code- Streamlit provides code for the build out of this app: https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps as well as the youtube video: https://www.youtube.com/watch?v=sBhK-2K9bUc&t=303s 
+#     2nd- from within the OpenAI asssitant creator"""
+#   )
 
-with st.expander("Assistant created within Streamlit"):
-    openai.api_key= st.secrets["OPENAI_API_KEY"]
-      # Initialize session state for openai_model
-    if "openai_model" not in st.session_state:
-      st.session_state["openai_model"] = "gpt-3.5-turbo"
+# with st.expander("Assistant created within Streamlit"):
+#     openai.api_key= st.secrets["OPENAI_API_KEY"]
+#       # Initialize session state for openai_model
+#     if "openai_model" not in st.session_state:
+#       st.session_state["openai_model"] = "gpt-3.5-turbo"
   
-  # Initialize session state for messages
-    if "messages" not in st.session_state:
-      st.session_state.messages = []
-  # Display existing messages
-    for message in st.session_state.messages:
-      with st.chat_message(message["role"]):
-          st.markdown(message["content"])
+#   # Initialize session state for messages
+#     if "messages" not in st.session_state:
+#       st.session_state.messages = []
+#   # Display existing messages
+#     for message in st.session_state.messages:
+#       with st.chat_message(message["role"]):
+#           st.markdown(message["content"])
 
-    if prompt:= st.chat_input("How's it hanging?"):
-      with st.chat_message("user"):
-          st.markdown(prompt)
-      st.session_state.messages.append({"role": "user", "content": prompt})
+#     if prompt:= st.chat_input("How's it hanging?"):
+#       with st.chat_message("user"):
+#           st.markdown(prompt)
+#       st.session_state.messages.append({"role": "user", "content": prompt})
       
-      with st.chat_message("assistant"):
-          message_placeholder = st.empty()
-          full_response = ""
-          for response in openai.ChatCompletion.create(
-              model=st.session_state["openai_model"],
-              messages=[
-                  {"role": m["role"], "content": m["content"]}
-                  for m in st.session_state.messages
-              ],
-              stream=True)
-          response = st.write_stream(stream)
-      st.session_state.messages.append({"role": "assistant", "content": response})
+#       with st.chat_message("assistant"):
+#           message_placeholder = st.empty()
+#           full_response = ""
+#           for response in openai.ChatCompletion.create(
+#               model=st.session_state["openai_model"],
+#               messages=[
+#                   {"role": m["role"], "content": m["content"]}
+#                   for m in st.session_state.messages
+#               ],
+#               stream=True)
+#           response = st.write_stream(stream)
+#       st.session_state.messages.append({"role": "assistant", "content": response})
 #_____________________________________________________________________________________________
 # Correct the API Key access
 #openai.api_key = st.secrets["OPENAI_API_KEY"]
